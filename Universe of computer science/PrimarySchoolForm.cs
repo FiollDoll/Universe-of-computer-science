33df2d298ff8d06dd6ca7a76d90f9beee27ad67f
FiollDoll <artemkadgi2@gmail.com>
2025-01-07 23:56:12 +0500

    PrimarySchoolForm prototype design

--- Universe of computer science/PrimarySchoolForm.cs
+++ Universe of computer science/PrimarySchoolForm.cs
@@ -6,4 +6,15 @@ public partial class PrimarySchoolForm : Form
     {
         InitializeComponent();
     }
+
+    private void ButtonReturn_Click(object sender, EventArgs e)
+    {
+        MainForm mainForm = new MainForm();
+        mainForm.StartPosition = FormStartPosition.Manual;
+        mainForm.Location = this.Location; // Назначение локации изначальной  формы
+        mainForm.FormClosed += (s, args) => this.Close(); // Чтобы всё грамотно закрывалось
+        this.Hide();
+
+        mainForm.Show();
+    }
 }
